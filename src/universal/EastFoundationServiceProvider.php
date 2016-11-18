8991970c787538f1565e38ed1522c8c288f5bf90
Richard Deloge <richarddeloge@gmail.com>
2016-11-18 23:51:16 +0100

    add service provider

---
+++ src/universal/EastFoundationServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * East Foundation.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\East\Foundation;
+
+use Interop\Container\ContainerInterface;
+use Interop\Container\ServiceProvider;
+use Psr\Log\LoggerInterface;
+use Teknoo\East\Foundation\Manager\ManagerInterface;
+use Teknoo\East\Foundation\Manager\Manager;
+use Teknoo\East\Foundation\Processor\Processor;
+use Teknoo\East\Foundation\Processor\ProcessorInterface;
+
+/**
+ * Definition provider following PSR 11 Draft to build an universal bundle/package.
+ */
+class EastFoundationServiceProvider implements ServiceProvider
+{
+    /**
+     * Constants to define services' keys in container.
+     */
+    const SERVICE_PROCESSOR = 'teknoo.east.processor';
+    const SERVICE_MANAGER = 'teknoo.east.manager';
+
+    /**
+     * @param ContainerInterface $container
+     * @return ProcessorInterface
+     */
+    public static function createProcessor(ContainerInterface $container): ProcessorInterface
+    {
+        return new Processor($container->get(LoggerInterface::class));
+    }
+
+    /**
+     * @return ManagerInterface
+     */
+    public static function createManager(): ManagerInterface
+    {
+        return new Manager();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getServices()
+    {
+        return [
+            //teknoo.states.lifecyclable.service.tokenizer
+            ProcessorInterface::class => [static::class, 'createProcessor'],
+            static::SERVICE_PROCESSOR => [static::class, 'createProcessor'],
+
+            //teknoo.states.lifecyclable.bridge.event_dispatcher
+            ManagerInterface::class => [static::class, 'createManager'],
+            static::SERVICE_MANAGER => [static::class, 'createManager'],
+        ];
+    }
+}
---
+++ tests/universal/EastFoundationServiceProviderTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * East Foundation.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\Tests\East\Foundation;
+
+use Interop\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Teknoo\East\Foundation\EastFoundationServiceProvider;
+use Teknoo\East\Foundation\Manager\ManagerInterface;
+use Teknoo\East\Foundation\Processor\ProcessorInterface;
+
+/**
+ * Class DefinitionProviderTest
+ * @covers \Teknoo\East\Foundation\EastFoundationServiceProvider
+ */
+class EastFoundationServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return EastFoundationServiceProvider
+     */
+    public function buildProvider(): EastFoundationServiceProvider
+    {
+        return new EastFoundationServiceProvider();
+    }
+
+    public function testGetDefinitions()
+    {
+        $definitions = $this->buildProvider()->getServices();
+        self::assertTrue(isset($definitions[ProcessorInterface::class]));
+        self::assertTrue(isset($definitions['teknoo.east.processor']));
+        self::assertTrue(isset($definitions[ManagerInterface::class]));
+        self::assertTrue(isset($definitions['teknoo.east.manager']));
+    }
+
+    public function testCreateManager()
+    {
+        self::assertInstanceOf(
+            ManagerInterface::class,
+            EastFoundationServiceProvider::createManager()
+        );
+    }
+
+    public function testCreateProcessor()
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects(self::any())
+            ->method('get')
+            ->withConsecutive([LoggerInterface::class])
+            ->willReturnOnConsecutiveCalls($this->createMock(LoggerInterface::class));
+
+        self::assertInstanceOf(
+            ProcessorInterface::class,
+            EastFoundationServiceProvider::createProcessor($container)
+        );
+    }
+}
