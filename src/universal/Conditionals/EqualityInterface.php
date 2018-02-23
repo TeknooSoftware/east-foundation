746ea6b04d1f33ff731ad0395114b8fb93f13793
Richard Deloge <richarddeloge@gmail.com>
2018-02-23 23:40:08 +0100

    Add interfaces to define objects able to test if two object are equals, lower or greater

---
+++ src/universal/Conditionals/EqualityInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * East Foundation.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\East\Foundation\Conditionals;
+
+use Teknoo\East\Foundation\Promise\PromiseInterface;
+
+/**
+ * Interface to define object able to test if an another object is equal to it, and pass the result to the promise.
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+interface EqualityInterface
+{
+    /**
+     * To define object able to test if an another object is equal to it, and pass the result to the promise.
+     *
+     * @param EqualityInterface|object|mixed $object
+     * @param PromiseInterface $promise
+     * @return HighInterface
+     */
+    public function isEqualTo($object, PromiseInterface $promise): HighInterface;
+}
---
+++ src/universal/Conditionals/LowInterface.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * East Foundation.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\East\Foundation\Conditionals;
+
+use Teknoo\East\Foundation\Promise\PromiseInterface;
+
+/**
+ * Interface to define object able to test if an another object is lower or equal then it, and pass the result
+ * to the promise.
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+interface LowInterface
+{
+    /**
+     * To define object able to test if an another object is lower or equal then it, and pass the result to the promise.
+     *
+     * @param LowInterface|object|mixed $object
+     * @param PromiseInterface $promise
+     * @return HighInterface
+     */
+    public function isLowerThan($object, PromiseInterface $promise): HighInterface;
+}
