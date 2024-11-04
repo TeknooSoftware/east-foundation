63b7ad653602b7479ef0a54606c68b66efdf2973
Richard Deloge <richarddeloge@gmail.com>
2024-11-04 08:05:01 +0100

    Simplify FQN in EastFoundationExtension.php

--- infrastructures/symfony/DependencyInjection/EastFoundationExtension.php
+++ infrastructures/symfony/DependencyInjection/EastFoundationExtension.php
@@ -29,7 +29,7 @@ use Exception;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -52,7 +52,7 @@ class EastFoundationExtension extends Extension
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
         return $this;
