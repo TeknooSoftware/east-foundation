e7b864839db7844fe8a1a5b12dfdc495027cadaa
Richard Deloge <richarddeloge@gmail.com>
2025-07-30 23:43:42 +0200

    license

--- tests/universal/Liveness/TimeoutServiceTest.php
+++ tests/universal/Liveness/TimeoutServiceTest.php
@@ -5,7 +5,7 @@
  *
  * LICENSE
  *
- * This source file is subject to the MIT license
+ * This source file is subject to the 3-Clause BSD license
  * it is available in LICENSE file at the root of this package
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
@@ -17,7 +17,7 @@
  *
  * @link        https://teknoo.software/east-collection/foundation Project website
  *
- * @license     https://teknoo.software/license/mit         MIT License
+ * @license     http://teknoo.software/license/bsd-3         3-Clause BSD License
  * @author      Richard Déloge <richard@teknoo.software>
  */
 
@@ -33,7 +33,7 @@ use Teknoo\East\Foundation\Liveness\TimeoutService;
 use Teknoo\East\Foundation\Time\TimerService;
 
 /**
- * @license     https://teknoo.software/license/mit         MIT License
+ * @license     http://teknoo.software/license/bsd-3         3-Clause BSD License
  * @author      Richard Déloge <richard@teknoo.software>
  */
 #[CoversClass(TimeoutService::class)]
